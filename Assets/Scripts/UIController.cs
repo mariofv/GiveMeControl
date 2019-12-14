0638b5dff79a80e3ed2acc61f884c87bc993d835
Mario Fernández Villalba <mario_bcn2@hotmail.es>
2019-12-14 18:13:36 +0100

    Move scripts to Scripts folder

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [Header("Level Completed")]
+    public GameObject levelCompletedPanel;
+    public Text levelCompletedText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowCompletedLevelScreen(int level)
+    {
+        levelCompletedText.text = "LEVEL " + level + " COMPLETE!";
+        levelCompletedPanel.SetActive(true);
+    }
+}
