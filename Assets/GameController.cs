d1a4e3b000a60b2fd69f9fa75933fe3ec7c8e723
Mario Fernández Villalba <mario_bcn2@hotmail.es>
2019-12-14 17:31:10 +0100

    Add UI base panel

---
+++ Assets/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Adventurers"), LayerMask.NameToLayer("Adventurers"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
