f697d361e723b803b7d5d26bbdb5e0ae4ed72931
Mario Fernández Villalba <mario_bcn2@hotmail.es>
2019-12-14 16:26:26 +0100

    Add selected player display

---
+++ Assets/Floater.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour
+{
+    [Header("Config")]
+    public float floatSpeed = 0;
+    public float floatAmplitude = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.parent.position + Vector3.up * (Mathf.Sin(Time.time * floatSpeed) * floatAmplitude + 0.75f);
+    }
+}
