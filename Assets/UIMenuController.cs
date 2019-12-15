a7fae62139e81a9984549527c93588a02b82ab7d
Mario Fernández Villalba <mario_bcn2@hotmail.es>
2019-12-15 18:14:35 +0100

    Add level selection menu

---
+++ Assets/UIMenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMenuController : MonoBehaviour
+{
+    public GameObject buttonCanvas;
+    public GameObject levelSelectionCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenLevelSelectionWindow()
+    {
+        buttonCanvas.SetActive(false);
+        levelSelectionCanvas.SetActive(true);
+    }
+
+    public void CloseLevelSelectionWindow()
+    {
+        buttonCanvas.SetActive(true);
+        levelSelectionCanvas.SetActive(false);
+    }
+}
